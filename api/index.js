5691dd7b993322874ddbcba30db21b625b02cb4e
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-03-23 22:23:43 +0500

    upaet

--- api/index.js
+++ api/index.js
@@ -5,17 +5,7 @@ const Student = require("../models/Student");
 const router = require('../router');
 const connectDatabse = require('../db/connection');
 const logger = require('../utils/logger');
-
-
-mongoose.connect('mongodb+srv://arslanmirza474:arslanmirza474@traffic-assessment.c65esoz.mongodb.net/Traffic-Assessment', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+connectDatabse();
 router.get("/api",(req,res)=>{
   res.send("this is working")
 })
@@ -102,19 +92,17 @@ router.get('/api/students/timestamps', async (req, res) => {
     res.status(500).json({ success: false, error: 'Internal server error' });
   }
 });
-app.listen(4000, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-// const server = router.listen(process.env.PORT, () => {
-//   console.clear();
-//   logger.info(`Server is port ${process.env.PORT} in ${process.env.NODE_ENV}`);
-//   logger.info(`http://localhost:${process.env.PORT}/`);
-// });
 
-// process.on('unhandledRejection', (err) => {
-//   logger.error(`Error name : ${err.name} , Error msg ${err.message}  `);
-//   logger.error('Shutting down Server due to  Rejection Errors');
-//   server.close(() => {
-//     process.exit();
-//   });
-// });
+const server = router.listen(process.env.PORT, () => {
+  console.clear();
+  logger.info(`Server is port ${process.env.PORT} in ${process.env.NODE_ENV}`);
+  logger.info(`http://localhost:${process.env.PORT}/`);
+});
+
+process.on('unhandledRejection', (err) => {
+  logger.error(`Error name : ${err.name} , Error msg ${err.message}  `);
+  logger.error('Shutting down Server due to  Rejection Errors');
+  server.close(() => {
+    process.exit();
+  });
+});
