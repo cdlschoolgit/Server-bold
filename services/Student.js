5a9fc6c59e85ee4c99c4aa9b3e59bcdbf827fded
Muhammad Saad Afzal <43127883+arslanmirza474@users.noreply.github.com>
2024-06-13 00:17:30 +0500

    udpate

--- services/Student.js
+++ services/Student.js
@@ -272,7 +272,7 @@ const createStudentWithDetails = catchAsyncErrors(
     const resetToken = studentCreated.getResetPasswordToken();
 
     // const SERVER_URL = "https://traffic-assessment.herokuapp.com";
-    const verifyURL = `https://server-bold.vercel.app/verifyStudent/?token=${resetToken}&name=${name}&email=${email}`;
+    const verifyURL = `https://traffic-assessment.herokuapp.com/verifyStudent/?token=${resetToken}&name=${name}&email=${email}`;
     sendEmail({
       userName: name,
       email: email,
